ad4bea0d0b4d32c55247eeef5543b4eaff3dd6b4
AnnaVin <anna@Annas-MacBook-Air.local>
2017-06-18 23:08:51 +0300

    something

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+/**
+ * Created by anna on 6/17/17.
+ */
+public class Rectangle {
+
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+
+    }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by anna on 6/17/17.
+ */
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+}
